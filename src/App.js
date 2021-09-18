6a74cd5c8b6fcab1f6033ab9d650ef602875a98d
salt4072 <hiro.salt1231@gmail.com>
2021-09-18 19:48:38 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
+// function App() {
+//   return (
+//     <div>hello world</div>
+//   );
+// }
+
+// class App extends Component {
+//   render() {
+//     // const greeting = "Hi! abc"
+//     // const dom = <h1 className="foo">{greeting}</h1>;
+//     // return dom;
+//     return (
+//       // <div>
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => { console.log("I am clicked .") }} ></input>
+//       </React.Fragment>
+//       // </div>
+//     )
+
+//   }
+
+// }
+
+// const App =()=>{
+
+//   return (
+//     <div>
+//       <Cat/>
+//       <Cat/>
+//       <Cat/>
+//       <Cat/>
+
+
+
+
+//     </div>
+//   )
+    
+// }
+// const Cat =()=>{
+//   return <div>Meow</div>
+// }
+
+// const App =()=>{
+//   return (
+//     <div>
+//       <User name = {"Taro"} age={10}/>
+//       <User name = {"hanako"} age={6}/>
+//     </div>
+//   )
+// }
+// const User =(props)=>{
+//   return <div>Hi!, I am {props.name}!, and {props.age}years old</div>
+// }
+
+const App =()=>{
+  const profiles = [
+    {name:"Taro",age:10},
+    {name:"hanako",age:5},
+    {name:"aaaaa",afe:5}
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>hello world</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        profiles.map((profile, index)=>{
+          return <User name={profile.name} age = {profile.age} key={index}/>
+        
+        })
+      }
     </div>
-  );
+  )
+    
 }
+const User =(props)=>{
+  return <div>Hi!, I am {props.name}!, and {props.age}years old</div>
+}
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
+}
+
+// User.defaultProps = {
+//   age:1
+// }
+
 
 export default App;
